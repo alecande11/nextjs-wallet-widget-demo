1197fa813a784078bbd8f04444d00bd6d9a9fddf
alecande11 <54709706+alecande11@users.noreply.github.com>
2025-03-10 11:51:06 +0100

    apply next config changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,16 @@
+import path from "path";
+
+const __dirname = path.resolve();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  poweredByHeader: false,
+
+  output: "standalone",
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, "../../"),
+    nextScriptWorkers: true,
+  },
   swcMinify: true,
 };
 
